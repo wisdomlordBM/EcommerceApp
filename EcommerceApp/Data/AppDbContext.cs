e692da760fcb58437f435ea0eaec6022687a8099
wisdomlord <wisdomlordbm@gmail.com>
2025-12-12 15:28:01 +0100

    Add project files.

---
+++ EcommerceApp/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EcommerceApp.Models;
+
+namespace EcommerceApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // DbSets
+        public DbSet<Product> Products { get; set; }
+    }
+}
+
